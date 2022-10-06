f0e2e1881c70799ed912c6ba38f6f418fbcbc7e8
Rajat <rajat.verma@innostax.com>
2022-10-06 13:41:01 +0530

    two bugs fixed

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -11,6 +11,4 @@ export interface State {}
 
 export const reducers: ActionReducerMap<State> = {};
 
-export const metaReducers: MetaReducer<State>[] = !environment.production
-  ? []
-  : [];
+export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
